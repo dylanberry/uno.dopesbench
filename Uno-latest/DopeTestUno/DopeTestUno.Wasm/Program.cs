eb56f8d043fe656b0292476cd054a61158f09fee
Dylan Berry <dylan@dylanberry.com>
2022-04-06 22:38:44 -0400

    fix references

--- Uno-latest/DopeTestUno/DopeTestUno.Wasm/Program.cs
+++ Uno-latest/DopeTestUno/DopeTestUno.Wasm/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.UI.Xaml;
+using Windows.UI.Xaml;
 
 namespace DopeTestUno.Wasm
 {
@@ -9,7 +9,7 @@ namespace DopeTestUno.Wasm
 
 		static int Main(string[] args)
 		{
-			Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
 
 			return 0;
 		}
